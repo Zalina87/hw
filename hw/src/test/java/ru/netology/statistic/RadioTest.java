626a04047e24e6dc5fd52eb0f254bd0c8bcf8c54
Zalina <salina87@yandex.ru>
2024-10-22 16:05:28 +0300

    Initial commit

---
+++ hw/src/test/java/ru/netology/statistic/RadioTest.java
@@ -0,0 +1,16 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void test() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(6);
+        int expec
+    }
+
+}
