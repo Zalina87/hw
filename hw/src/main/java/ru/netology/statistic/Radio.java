0fc64a3abbab1e8fd1872516dc9f0a9741b871f8
Zalina <salina87@yandex.ru>
2024-10-24 16:05:33 +0300

    Initial commit

--- hw/src/main/java/ru/netology/statistic/Radio.java
+++ hw/src/main/java/ru/netology/statistic/Radio.java
@@ -3,6 +3,33 @@ package ru.netology.statistic;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
+
+    public Radio() {
+        this.maxStation = 9;
+
+    }
+
+    public Radio(int stationsCount) {
+        this.maxStation = stationsCount - 1;
+
+    }
+
+    public void next() {
+        if (currentStation != maxStation) {
+            currentStation++;
+            return;
+        }
+        currentStation = 0;
+    }
+
+    public void prev() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = maxStation;
+        }
+    }
 
     public int getCurrentStation() {
         return currentStation;
@@ -12,7 +39,7 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
 
--- hw/src/test/java/ru/netology/statistic/RadioTest.java
+++ hw/src/test/java/ru/netology/statistic/RadioTest.java
@@ -1,5 +1,6 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +9,12 @@ class RadioTest {
 
     @Test
     public void test() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(6);
-        int expec
+        Radio radio = new Radio(20);
+        radio.setCurrentStation(15);
+
+        int expected = 15;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
     }
 
 }
